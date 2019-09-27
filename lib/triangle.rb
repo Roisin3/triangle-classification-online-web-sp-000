622a416f9d4952952183174d08efab1ae5f3f349
Michael Lynch <michael@michaelrlynch.com>
2019-09-27 17:16:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,7 @@ class Triangle
   end
   
   def kind
+    validate
     if side_one == side_two && side_two == side_three
       :equilateral
     elsif side_one == side_two || side_one == side_three || side_two == side_three
