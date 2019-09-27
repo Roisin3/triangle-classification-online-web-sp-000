fa87252087cb0009c8ad65e73dff7f76a7339d90
Michael Lynch <michael@michaelrlynch.com>
2019-09-27 17:12:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
   def validate
     triangle = [(side_one + side_two > side_three), (side_one + side_three > side_two), (side_two + side_three > side_one)]
     [side_one, side_two, side_three].each { |x| triangle << false if x <= 0 }
-    raise TriangleError if triangle.include?(false)
+      raise TriangleError if triangle.include?(false)
   end
   
   class TriangleError < StandardError
